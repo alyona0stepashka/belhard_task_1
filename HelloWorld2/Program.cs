6cbf2c73c8c9963c589ec836766a924e3fada697
alyona0stepashka <alyona0stepashka@users.noreply.github.com>
2020-11-27 22:02:04 +0300

    first commit

---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (i == 5)
+                { 
+                    continue; 
+                }
+                //break;   
+                System.Console.WriteLine(i);
+            }
+
+            System.Console.ReadLine();
+        }
+    }
+}
